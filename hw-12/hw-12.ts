5c105cb7270d356f1c76eabbf183c3a80e643723
Hieorhiu Yurii <j.hieorhiu@autodoc.eu>
2024-08-11 13:12:39 +0200

    added hw-12

--- hw-12/hw-12.ts
+++ hw-12/hw-12.ts
@@ -54,7 +54,37 @@ class MovieList {
     this.filterMovies();
   }
 
-  private filterMovies() {}
+  private filterMovies() {
+    let filteredMovies = this.movies;
+
+    if (this.filters.title) {
+      filteredMovies = filteredMovies.filter(movie =>
+        movie.title.includes(this.filters.title!.filter)
+      );
+    }
+
+    if (this.filters.releaseYear) {
+      filteredMovies = filteredMovies.filter(movie =>
+        movie.releaseYear >= this.filters.releaseYear!.filter &&
+        movie.releaseYear <= this.filters.releaseYear!.filterTo
+      );
+    }
+
+    if (this.filters.rating) {
+      filteredMovies = filteredMovies.filter(movie =>
+        movie.rating >= this.filters.rating!.filter &&
+        movie.rating <= this.filters.rating!.filterTo
+      );
+    }
+
+    if (this.filters.awards) {
+      filteredMovies = filteredMovies.filter(movie =>
+        this.filters.awards!.values.some(award => movie.awards.includes(award))
+      );
+    }
+
+    this.movies = filteredMovies;
+  }
 
   getFilteredMovies(): Movie[] {
     return this.movies;
@@ -80,7 +110,25 @@ class CategoryList {
     this.filterCategories();
   }
 
-  private filterCategories() {}
+  private filterCategories() {
+    let filteredCategories = this.categories;
+
+    if (this.filters.name) {
+      filteredCategories = filteredCategories.filter(category =>
+        category.name.includes(this.filters.name!.filter)
+      );
+    }
+
+    if (this.filters.movies) {
+      filteredCategories = filteredCategories.map(category => {
+        const movieList = new MovieList(category.movies);
+        movieList.applyFilters(this.filters.movies!);
+        return { ...category, movies: movieList.getFilteredMovies() };
+      }).filter(category => category.movies.length > 0);
+    }
+
+    this.categories = filteredCategories;
+  }
 
   getFilteredCategories(): Category[] {
     return this.categories;
